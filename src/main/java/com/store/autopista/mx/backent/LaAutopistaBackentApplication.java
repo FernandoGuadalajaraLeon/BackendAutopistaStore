4a99d23288c7e5ad8e0416e1200ce18e0eba6e7b
Fernando Guadalajara <f.guadalajara.l@gmail.com>
2023-06-28 15:24:05 -0600

    first commit autopista store backend

---
+++ src/main/java/com/store/autopista/mx/backent/LaAutopistaBackentApplication.java
@@ -0,0 +1,13 @@
+package com.store.autopista.mx.backent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaAutopistaBackentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaAutopistaBackentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/autopista/mx/backent/LaAutopistaBackentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.autopista.mx.backent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaAutopistaBackentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
